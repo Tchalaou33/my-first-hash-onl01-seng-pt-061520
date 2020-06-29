8b5257d2c125473bab817a88f4b0331477ec8354
Andrea Tchalaou <attchalaou@gmail.com>
2020-06-29 07:05:12 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,7 +23,6 @@ def retrieval
      shipping_manifest["oil paintings"]  
      
 end
-# shipping_manifest["oil paintings"] 
 
 def adding
   shipping_manifest = {
@@ -32,9 +31,9 @@ def adding
                   "oil paintings" => 3
                  }
                  
-  shipping_manifest["muskets"]="2"
+  shipping_manifest["muskets"]=2
 
-  shipping_manifest["gun powder"]="4"  
+  shipping_manifest["gun powder"]=4  
 
   shipping_manifest
   
