e15efb75cb02efabc509476a1f0b541941d0caaf
Andrea Tchalaou <attchalaou@gmail.com>
2020-06-29 07:01:46 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -20,10 +20,10 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-    # shipping_manifest["oil_paintings"]  
+     shipping_manifest["oil paintings"]  
      
 end
- shipping_manifest["oil paintings"] 
+# shipping_manifest["oil paintings"] 
 
 def adding
   shipping_manifest = {
